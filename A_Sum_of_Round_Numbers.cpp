fca19d754bf37cd97b4b4c254c69d5f6bda6ddbc
Jitesh Kumar <jiteshece@gmail.com>
2024-12-03 14:43:49 +0530

    800-1200

--- A_Sum_of_Round_Numbers.cpp
+++ A_Sum_of_Round_Numbers.cpp
@@ -8,71 +8,32 @@ using namespace std;
 #define ff first
 #define ss second
 
-bool isRoundNum(int num){
-    // cout<<"NUM ==>"<<num<< " "<<endl;
-    string currNum = to_string(num);
-    int digit = currNum[0] - '0';
-    // cout<<"DIGIT->>"<<digit<<endl;
 
-    if(digit == '0'){
-        return false;
-    }
-    if(digit <= 1 && digit >= 9){
-        return false;
-    }
-    for(int i = 1; i < currNum.size(); ++i){
-        // int currDigit = currNum[i] - '0';
-        if(currNum[i] != 48)
-            return false;
-    }
-    return true;
-}
 void solve(){
     int num;
     cin>>num;
 
-    vector<int> arr(10000, 0);
-    if(isRoundNum(num)){
-        // cout<<1<<endl;
-        // cout<<num<<endl
-    }
+    vector<int> arr;
 
-    int deci = 1;
-    int count = 0;
+    int j = 0;
     while(num>0){
         int digit = num % 10;
-        // cout<<"DIGIT=>"<<digit<<" ";
-        if(digit != 0){
-            int checkRoundNum = digit*deci;
-            // cout<<endl;
-            // cout<<"RoundNo:-->"<<checkRoundNum<<" ";
-            // cout<<endl;
-            if(isRoundNum(checkRoundNum)){
-
-                // cout<<"OLD COUNT==>"<<count<<endl;
-                count++;
-                arr[digit] = checkRoundNum;
-                // cout<<"NEW count==>"<<count<<endl;
-                // cout<<"------------------"<<endl;
-            }
+        if(digit){
+            int ans = pow(10,j);
+            ans *= digit;
+            arr.push_back(ans);
         }
-        // cout<<digit*deci<<" ";
-
-        deci *= 10;
-        num = num / 10;
+        num /= 10;
+        ++j;
     }
-    cout<<count<<endl;
-    // cout<<"count=>"<<count<<endl;
 
-    for(int i = 0; i < arr.size(); ++i){
-        if(arr[i] != 0){
-            cout<<arr[i]<<" ";
+    int len = arr.size();
+    cout<<len<<endl;
 
-        }
-
+    for(int i = 0; i < len; ++i){
+        cout<<arr[i]<<" ";
     }
     cout<<endl;
-
 
 }
 
